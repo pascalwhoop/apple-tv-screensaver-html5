ef50646752ab1d3c64970d63a4eeb39a65e165ce
Pascal Brokmeier <pascal.brokmeier@gmail.com>
2016-05-11 20:45:05 +1000

    init

---
+++ script.js
@@ -0,0 +1,402 @@
+var data = [
+    {
+        "id": "b1",
+        "assets": [
+            {
+                "url": "http://a1.phobos.apple.com/us/r1000/000/Features/atv/AutumnResources/videos/b1-1.mov",
+                "accessibilityLabel": "Hawaii",
+                "type": "video",
+                "id": "b1-1",
+                "timeOfDay": "day"
+            },
+            {
+                "url": "http://a1.phobos.apple.com/us/r1000/000/Features/atv/AutumnResources/videos/b1-2.mov",
+                "accessibilityLabel": "London",
+                "type": "video",
+                "id": "b1-2",
+                "timeOfDay": "night"
+            },
+            {
+                "url": "http://a1.phobos.apple.com/us/r1000/000/Features/atv/AutumnResources/videos/b1-3.mov",
+                "accessibilityLabel": "New York City",
+                "type": "video",
+                "id": "b1-3",
+                "timeOfDay": "day"
+            },
+            {
+                "url": "http://a1.phobos.apple.com/us/r1000/000/Features/atv/AutumnResources/videos/b1-4.mov",
+                "accessibilityLabel": "San Francisco",
+                "type": "video",
+                "id": "b1-4",
+                "timeOfDay": "night"
+            }
+        ]
+    },
+    {
+        "id": "b2",
+        "assets": [
+            {
+                "url": "http://a1.phobos.apple.com/us/r1000/000/Features/atv/AutumnResources/videos/b2-1.mov",
+                "accessibilityLabel": "China",
+                "type": "video",
+                "id": "b2-1",
+                "timeOfDay": "day"
+            },
+            {
+                "url": "http://a1.phobos.apple.com/us/r1000/000/Features/atv/AutumnResources/videos/b2-2.mov",
+                "accessibilityLabel": "Hawaii",
+                "type": "video",
+                "id": "b2-2",
+                "timeOfDay": "day"
+            },
+            {
+                "url": "http://a1.phobos.apple.com/us/r1000/000/Features/atv/AutumnResources/videos/b2-3.mov",
+                "accessibilityLabel": "New York City",
+                "type": "video",
+                "id": "b2-3",
+                "timeOfDay": "night"
+            },
+            {
+                "url": "http://a1.phobos.apple.com/us/r1000/000/Features/atv/AutumnResources/videos/b2-4.mov",
+                "accessibilityLabel": "San Francisco",
+                "type": "video",
+                "id": "b2-4",
+                "timeOfDay": "night"
+            }
+        ]
+    },
+    {
+        "id": "b3",
+        "assets": [
+            {
+                "url": "http://a1.phobos.apple.com/us/r1000/000/Features/atv/AutumnResources/videos/b3-1.mov",
+                "accessibilityLabel": "London",
+                "type": "video",
+                "id": "b3-1",
+                "timeOfDay": "night"
+            },
+            {
+                "url": "http://a1.phobos.apple.com/us/r1000/000/Features/atv/AutumnResources/videos/b3-2.mov",
+                "accessibilityLabel": "New York City",
+                "type": "video",
+                "id": "b3-2",
+                "timeOfDay": "day"
+            },
+            {
+                "url": "http://a1.phobos.apple.com/us/r1000/000/Features/atv/AutumnResources/videos/b3-3.mov",
+                "accessibilityLabel": "San Francisco",
+                "type": "video",
+                "id": "b3-3",
+                "timeOfDay": "day"
+            }
+        ]
+    },
+    {
+        "id": "b4",
+        "assets": [
+            {
+                "url": "http://a1.phobos.apple.com/us/r1000/000/Features/atv/AutumnResources/videos/b4-1.mov",
+                "accessibilityLabel": "Hawaii",
+                "type": "video",
+                "id": "b4-1",
+                "timeOfDay": "day"
+            },
+            {
+                "url": "http://a1.phobos.apple.com/us/r1000/000/Features/atv/AutumnResources/videos/b4-2.mov",
+                "accessibilityLabel": "New York City",
+                "type": "video",
+                "id": "b4-2",
+                "timeOfDay": "night"
+            },
+            {
+                "url": "http://a1.phobos.apple.com/us/r1000/000/Features/atv/AutumnResources/videos/b4-3.mov",
+                "accessibilityLabel": "San Francisco",
+                "type": "video",
+                "id": "b4-3",
+                "timeOfDay": "day"
+            }
+        ]
+    },
+    {
+        "id": "b5",
+        "assets": [
+            {
+                "url": "http://a1.phobos.apple.com/us/r1000/000/Features/atv/AutumnResources/videos/b5-1.mov",
+                "accessibilityLabel": "China",
+                "type": "video",
+                "id": "b5-1",
+                "timeOfDay": "day"
+            },
+            {
+                "url": "http://a1.phobos.apple.com/us/r1000/000/Features/atv/AutumnResources/videos/b5-2.mov",
+                "accessibilityLabel": "London",
+                "type": "video",
+                "id": "b5-2",
+                "timeOfDay": "day"
+            },
+            {
+                "url": "http://a1.phobos.apple.com/us/r1000/000/Features/atv/AutumnResources/videos/b5-3.mov",
+                "accessibilityLabel": "San Francisco",
+                "type": "video",
+                "id": "b5-3",
+                "timeOfDay": "night"
+            }
+        ]
+    },
+    {
+        "id": "b6",
+        "assets": [
+            {
+                "url": "http://a1.phobos.apple.com/us/r1000/000/Features/atv/AutumnResources/videos/b6-1.mov",
+                "accessibilityLabel": "China",
+                "type": "video",
+                "id": "b6-1",
+                "timeOfDay": "day"
+            },
+            {
+                "url": "http://a1.phobos.apple.com/us/r1000/000/Features/atv/AutumnResources/videos/b6-2.mov",
+                "accessibilityLabel": "Hawaii",
+                "type": "video",
+                "id": "b6-2",
+                "timeOfDay": "night"
+            },
+            {
+                "url": "http://a1.phobos.apple.com/us/r1000/000/Features/atv/AutumnResources/videos/b6-3.mov",
+                "accessibilityLabel": "London",
+                "type": "video",
+                "id": "b6-3",
+                "timeOfDay": "day"
+            },
+            {
+                "url": "http://a1.phobos.apple.com/us/r1000/000/Features/atv/AutumnResources/videos/b6-4.mov",
+                "accessibilityLabel": "San Francisco",
+                "type": "video",
+                "id": "b6-4",
+                "timeOfDay": "night"
+            }
+        ]
+    },
+    {
+        "id": "b7",
+        "assets": [
+            {
+                "url": "http://a1.phobos.apple.com/us/r1000/000/Features/atv/AutumnResources/videos/b7-1.mov",
+                "accessibilityLabel": "Hawaii",
+                "type": "video",
+                "id": "b7-1",
+                "timeOfDay": "day"
+            },
+            {
+                "url": "http://a1.phobos.apple.com/us/r1000/000/Features/atv/AutumnResources/videos/b7-2.mov",
+                "accessibilityLabel": "New York City",
+                "type": "video",
+                "id": "b7-2",
+                "timeOfDay": "day"
+            },
+            {
+                "url": "http://a1.phobos.apple.com/us/r1000/000/Features/atv/AutumnResources/videos/b7-3.mov",
+                "accessibilityLabel": "San Francisco",
+                "type": "video",
+                "id": "b7-3",
+                "timeOfDay": "night"
+            }
+        ]
+    },
+    {
+        "id": "b8",
+        "assets": [
+            {
+                "url": "http://a1.phobos.apple.com/us/r1000/000/Features/atv/AutumnResources/videos/b10-4.mov",
+                "accessibilityLabel": "San Francisco",
+                "type": "video",
+                "id": "b10-4",
+                "timeOfDay": "night"
+            },
+            {
+                "url": "http://a1.phobos.apple.com/us/r1000/000/Features/atv/AutumnResources/videos/b8-2.mov",
+                "accessibilityLabel": "San Francisco",
+                "type": "video",
+                "id": "b8-2",
+                "timeOfDay": "day"
+            },
+            {
+                "url": "http://a1.phobos.apple.com/us/r1000/000/Features/atv/AutumnResources/videos/b8-3.mov",
+                "accessibilityLabel": "San Francisco",
+                "type": "video",
+                "id": "b8-3",
+                "timeOfDay": "day"
+            }
+        ]
+    },
+    {
+        "id": "b9",
+        "assets": [
+            {
+                "url": "http://a1.phobos.apple.com/us/r1000/000/Features/atv/AutumnResources/videos/b9-1.mov",
+                "accessibilityLabel": "Hawaii",
+                "type": "video",
+                "id": "b9-1",
+                "timeOfDay": "day"
+            },
+            {
+                "url": "http://a1.phobos.apple.com/us/r1000/000/Features/atv/AutumnResources/videos/b9-2.mov",
+                "accessibilityLabel": "London",
+                "type": "video",
+                "id": "b9-2",
+                "timeOfDay": "night"
+            },
+            {
+                "url": "http://a1.phobos.apple.com/us/r1000/000/Features/atv/AutumnResources/videos/b9-3.mov",
+                "accessibilityLabel": "San Francisco",
+                "type": "video",
+                "id": "b9-3",
+                "timeOfDay": "day"
+            }
+        ]
+    },
+    {
+        "id": "b10",
+        "assets": [
+            {
+                "url": "http://a1.phobos.apple.com/us/r1000/000/Features/atv/AutumnResources/videos/b10-1.mov",
+                "accessibilityLabel": "Hawaii",
+                "type": "video",
+                "id": "b10-1",
+                "timeOfDay": "day"
+            },
+            {
+                "url": "http://a1.phobos.apple.com/us/r1000/000/Features/atv/AutumnResources/videos/b10-2.mov",
+                "accessibilityLabel": "New York City",
+                "type": "video",
+                "id": "b10-2",
+                "timeOfDay": "night"
+            },
+            {
+                "url": "http://a1.phobos.apple.com/us/r1000/000/Features/atv/AutumnResources/videos/b10-3.mov",
+                "accessibilityLabel": "San Francisco",
+                "type": "video",
+                "id": "b10-3",
+                "timeOfDay": "day"
+            },
+            {
+                "url": "http://a1.phobos.apple.com/us/r1000/000/Features/atv/AutumnResources/videos/b8-1.mov",
+                "accessibilityLabel": "Hawaii",
+                "type": "video",
+                "id": "b8-1",
+                "timeOfDay": "night"
+            }
+        ]
+    }
+];
+
+
+var fetchData = function () {
+    $.ajax({
+        url: "http://a1.phobos.apple.com/us/r1000/000/Features/atv/AutumnResources/videos/entries.json",
+        context: document.body
+    }).done(function (data) {
+        $("#debug").innerHTML = data;
+    });
+};
+
+var app = {
+    flipping: false,
+    currentIndex: 0
+};
+
+
+function init() {
+    app.allVidsRandom = getAllVidsRandom();
+    app.vid1 = document.getElementById("video1");
+    app.vid2 = document.getElementById("video2");
+
+    hookVideoEvents();
+    startPlay();
+}
+
+function startPlay() {
+    app.vid1.src = getNextVideo();
+    app.vid1.play();
+}
+
+function flipVids(current, next) {
+    console.log("flipping " + current.id + " for " + next.id);
+    app.flipping = true;
+    next.src = getNextVideo();
+    next.play();
+    current.className = "hide";
+    next.className = "show";
+
+    setTimeout(function(){
+        app.flipping = false;
+        current.pause();
+    }, 10000); //10000ms so 7s buffer for animation
+}
+
+function hookVideoEvents() {
+
+    /*//when vid1 ends
+    app.vid1.addEventListener('ended', function (e) {
+        flipVids(app.vid1, app.vid2)
+    }, false);
+
+
+    //when vid2 ends
+    app.vid2.addEventListener('ended', function (e) {
+        flipVids(app.vid2, app.vid1)
+    }, false);*/
+
+    //timing functions
+    app.vid1.addEventListener(
+        "timeupdate",
+        function (event) {
+            var dur = event.target.duration;
+            var curr = event.target.currentTime;
+            if(dur-curr < 6 && !app.flipping)flipVids(app.vid1, app.vid2);
+            else console.log("wait! " + (dur -curr).toString());
+
+        });
+
+    //timing functions
+    app.vid2.addEventListener(
+        "timeupdate",
+        function (event) {
+            var dur = event.target.duration;
+            var curr = event.target.currentTime;
+            if(dur-curr < 6 && !app.flipping) flipVids(app.vid2, app.vid1);
+            else console.log("wait! " + (dur -curr).toString());
+
+        });
+
+}
+
+
+function getAllVidsRandom() {
+    var vidUrls = [];
+    for (var i = 0; i < data.length; i++) {
+        for (var j = 0; j < data[i].assets.length; j++) {
+            vidUrls.push(data[i].assets[j].url);
+        }
+    }
+    return shuffle(vidUrls);
+}
+
+function getNextVideo() {
+    if (app.currentIndex >= app.allVidsRandom.length) {
+        app.currentIndex = 0;
+    }
+    return app.allVidsRandom[++app.currentIndex];
+}
+
+function shuffle(a) {
+    var j, x, i;
+    for (i = a.length; i; i -= 1) {
+        j = Math.floor(Math.random() * i);
+        x = a[i - 1];
+        a[i - 1] = a[j];
+        a[j] = x;
+    }
+    return a;
+}
+
+$(document).ready(init);
